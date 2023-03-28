994d9d72c0fe2dcc8f94730ea51947201ec444b6
Yunuen AM <yunameza@outlook.com>
2023-03-28 14:11:59 -0600

    Feat: CRUD implementation added

--- controllers/app.js
+++ controllers/app.js
@@ -7,33 +7,30 @@ app.use(bodyParser.json());
 
 let characters = [];
 
-app.post('/characters', function(req, res) {
-  const newCharacter = { ...req.body, id: characters.length + 1 }
-  characters = [ ...characters, newCharacter]
-  res.json(newCharacter);
-});
-
-app.put('/characters', function(req, res) {
-  let updatedCharacter;
-  characters = characters.map(p => {
-    if (p.id === req.body.id) {
-      updatedCharacter = { ...p, ...req.body };
-      return updatedCharacter;
-    }
-    return p;
-  })
-  res.json(updatedCharacter);
-});
-
-app.delete('/characters/:id', function(req, res) {
-  const deletedCharacter = characters.find(p => p.id === +req.params.id);
-  characters = characters.filter(p => p.id !== +req.params.id);
-  res.json(deletedCharacter);
-});
-
-app.get('/characters', (req, res) => {
-    res.json(characters);
-  })
+app.route('/characters')
+ .get((req, res) => {
+   res.json(characters);
+ })
+ .post((req, res) => {
+   const newCharacter = { ...req.body, id: characters.length + 1 }
+   characters = [...characters, newCharacter]
+   res.json(newCharacter);
+ })
+.put((req, res) => {
+   let updatedCharacter;
+   characters = characters.map(p => {
+     if (p.id === req.body.id) {
+       updatedCharacter = { ...p, ...req.body };
+       return updatedCharacter;
+     }
+     return p;
+   })
+   res.json(updatedCharacter);
+ })
+ .delete((req, res) => {
+   const deletedCharacter = characters.find(p => p.id === +req.body.id);
+   characters = characters.filter(p => p.id !== +req.body.id);
+   res.json(deletedCharacter);
+ })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-  
